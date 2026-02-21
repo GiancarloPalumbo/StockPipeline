df61def87d02cbfa9a4389ab711ce3983a43ffbd
Giancarlo Palumbo <113045664+GiancarloPalumbo@users.noreply.github.com>
2026-02-20 19:51:27 -0500

    Update copy_redshift.sql
    Changed Bucket name and IAM URI

--- sql-files/copy_redshift.sql
+++ sql-files/copy_redshift.sql
@@ -1,5 +1,5 @@
 COPY stock_prices
-FROM 's3://stock-pipeline-bucket1/processed/'
-IAM_ROLE 'arn:aws:iam::343218202970:role/redshift-s3-role'
+FROM 's3://My-Bucket-Name/processed/'
+IAM_ROLE 'arn:aws:iam::142431531412:role/redshift-s3-role'
 CSV
 IGNOREHEADER 1;
